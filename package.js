a0bbefd1c7117df65f2a837b889ff1889d83a325
Nazar Leush <n.leush@gmail.com>
2014-11-27 17:26:53 +0200

    allow setup widget setup and api endpoint

--- package.js
+++ package.js
@@ -1,24 +1,27 @@
 Package.describe({
-  name: 'iframely:oembed',
-  summary: 'Easy insert oEmbed widget in template',
-  version: '0.0.1',
-  git: 'https://github.com/itteco/meteor-oembed.git'
+    name: 'iframely:oembed',
+    summary: 'Easy insert oEmbed widget in template',
+    version: '0.0.1',
+    git: 'https://github.com/itteco/meteor-oembed.git'
 });
 
 Package.onUse(function(api) {
 
-  api.versionsFrom('METEOR@0.9.2');
+    api.versionsFrom('METEOR@0.9.2');
 
-  api.use('templating');
-  api.use('http');
+    api.use('templating');
+    api.use('http');
 
-  api.addFiles('lib/client/base-widget.html', 'client');
-  api.addFiles('lib/client/base-widget.js', 'client');
-  api.addFiles('lib/client/base-widget.css', 'client');
+    api.addFiles('lib/client/base-widget.html', 'client');
+    api.addFiles('lib/client/base-widget.js', 'client');
+    api.addFiles('lib/client/base-widget.css', 'client');
 
-  api.addFiles('lib/client/oembed.html', 'client');
-  api.addFiles('lib/client/oembed.js', 'client');
+    api.addFiles('lib/client/oembed.html', 'client');
+    api.addFiles('lib/client/oembed.js', 'client');
 
-  api.addFiles('lib/server/oembed.js', 'server');
+    api.addFiles('lib/server/oembed.js', 'server');
 
+    api.addFiles('lib/settings.js');
+
+    api.export('IframelyOembed');
 });
