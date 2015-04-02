192b9f3a545aea01921e5a12e123d263a58928d9
Nazar Leush <n.leush@gmail.com>
2015-04-02 23:12:59 +0300

    update version to 0.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'iframely:oembed',
     summary: 'Easy insert oEmbed widget in template',
-    version: '0.0.1',
+    version: '0.0.2',
     git: 'https://github.com/itteco/meteor-oembed.git'
 });
 
